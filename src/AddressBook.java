3fc567c2fc28df6e21d0cd563c003fe19b6aee2b
leena <leenaford@cmail.carleton.ca>
2023-09-17 01:18:10 -0400

    Added "Address Book" output.

---
+++ src/AddressBook.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+
+/***
+ * Class containing collection of BuddyInfo objects
+ *
+ * @author Leena Ford 101229281
+ * @version September 17, 2023
+ */
+
+public class AddressBook {
+
+    // collection of BuddyInfo objects
+    private ArrayList <BuddyInfo> buddiesCollection;
+
+    // Constructor
+    public AddressBook() {
+        buddiesCollection = new ArrayList<BuddyInfo>();
+    }
+
+    // Add a BuddyInfo object to the collection
+    public void addBuddy(BuddyInfo buddyToAdd) {
+        buddiesCollection.add(buddyToAdd);
+    }
+
+    // Remove a BuddyInfo object from collection
+    public void removeBuddy(BuddyInfo buddyToRemove) {
+        buddiesCollection.remove(buddyToRemove);
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Address Book");
+    }
+
+}
